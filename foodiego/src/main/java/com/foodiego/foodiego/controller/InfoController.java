925abe2ccc830d96d3de3082849d90fff86d0465
Prashanth Pothugunta <pothuguntaprashanth84@gmail.com>
2026-07-07 13:54:11 +0530

    Added about section

---
+++ foodiego/src/main/java/com/foodiego/foodiego/controller/InfoController.java
@@ -0,0 +1,28 @@
+package com.foodiego.foodiego.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class InfoController {
+
+    @GetMapping("/about")
+    public String about() {
+        return "about";
+    }
+
+    @GetMapping("/privacy")
+    public String privacy() {
+        return "privacy";
+    }
+
+    @GetMapping("/terms")
+    public String terms() {
+        return "terms";
+    }
+
+    @GetMapping("/contact")
+    public String contact() {
+        return "contact";
+    }
+}
